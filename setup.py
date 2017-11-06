ff97300b1f2ef30d6882b230b3e131c76e19bd75
Viet Le <vietlq85@gmail.com>
2017-11-06 22:17:15 +0000

    Fixed import of cryptux - v0.0.3

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
 # Read https://github.com/django-extensions/django-extensions/issues/92
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
 # Read: http://setuptools.readthedocs.io/en/latest/setuptools.html
 # Look for find_packages, packages, package_dir
+from setuptools import setup, find_packages
 
 setup(name='cryptux',
-      version='0.0.2',
+      version='0.0.3',
       description='Simple wallet for Cryptocurrencies',
       author='Viet Le',
       author_email='vietlq85@gmail.com',
       url='https://github.com/VISCHub/cryptux',
       install_requires=['ecdsa>=0.13'],
-      packages=['src/cryptux'],
+      packages=find_packages('src'),  # include all packages under src
+      package_dir={'' : 'src'},   # tell distutils packages are under src
       scripts=['tools/cryptux'],
       keywords=['crypto', 'hdw', 'wallet', 'bitcoin', 'ether'],
       classifiers=[])
