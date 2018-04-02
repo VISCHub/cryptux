ab475dee8ae70e5069c822426175e6e1a1373b2a
Viet Le <vietlq85@gmail.com>
2018-04-02 10:31:57 +0100

    shorten code

--- cryptux/bitcoin/hashes.py
+++ cryptux/bitcoin/hashes.py
@@ -3,14 +3,12 @@ import hashlib
 
 def hash160(in_bytes):
     '''Performs RIPEMD160(SHA256(in_bytes)) and returns raw digest'''
-    sha256_hasher = hashlib.sha256(in_bytes)
     ripemd160_hasher = hashlib.new('ripemd160')
-    ripemd160_hasher.update(sha256_hasher.digest())
+    ripemd160_hasher.update(hashlib.sha256(in_bytes).digest())
     return ripemd160_hasher.digest()
 
 
 def hash256(in_bytes):
     '''Performs SHA256(SHA256(in_bytes)) and returns raw digest'''
-    sha256_hasher = hashlib.sha256(in_bytes)
-    sha256_hasher2 = hashlib.sha256(sha256_hasher.digest())
-    return sha256_hasher2.digest()
+    sha256 = hashlib.sha256
+    return sha256(sha256(in_bytes).digest()).digest()
